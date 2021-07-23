7be94ab44aec219d577962ca41a128e6773de0df
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-07-23 15:51:05 -0400

    counter made and complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const decrement = () => {
+    if (count > -10) {
+      setCount(count - 1);
+    }
+  };
+
+  const increment = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Item Counter</h1>
+      <div>
+        <button onClick={increment}>Plus</button>
+        <button onClick={decrement}>Minus</button>
+      </div>
+      <div>{count}</div>
     </div>
   );
 }
